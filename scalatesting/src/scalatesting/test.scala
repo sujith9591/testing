4f761d7f92d4e8d61300dfd025a9ac81ea61a959
Sujith <sujithkumarcse@gmail.com>
2019-02-22 17:10:50 +0530

    updated

--- scalatesting/src/scalatesting/test.scala
+++ scalatesting/src/scalatesting/test.scala
@@ -4,6 +4,6 @@ object test {
   def main(args:Array[String])
   {
     println("basic scala program");
-    println("second line updated ok last ok last")
+    println("second line updated ok last ok last and more")
   }
 }
